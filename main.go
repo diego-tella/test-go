db795abb1d47fc26cb43a5aa1231d777b87fa2b6
Diego Tellaroli <diegotellaroli05@gmail.com>
2024-09-21 14:09:13 -0300

    Update main.go

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "io/ioutil"
+    "log"
+)
 
 func main() {
-	fmt.Println("Vivo!")
+    // Nome do arquivo a ser lido
+    filename := "/etc/passwd"
+
+    // Lê o conteúdo do arquivo
+    content, err := ioutil.ReadFile(filename)
+    if err != nil {
+        log.Fatalf("Erro ao ler o arquivo %s: %v", filename, err)
+    }
+
+    // Converte o conteúdo para string e imprime
+    fmt.Println(string(content))
 }
